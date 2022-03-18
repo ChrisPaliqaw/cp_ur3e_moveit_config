92609ee6f4c51ec0b6a3dbe42dd08d799afec645
Chris Paliqaw <christophomos@gmail.com>
2022-03-18 16:07:45 +0000

    Simple

---
+++ scripts/pick_place.py
@@ -0,0 +1,34 @@
+#! /usr/bin/env python
+
+import sys
+import copy
+import rospy
+import moveit_commander
+import moveit_msgs.msg
+import geometry_msgs.msg
+
+moveit_commander.roscpp_initialize(sys.argv)
+rospy.init_node('pick_place', anonymous=True)
+
+robot = moveit_commander.RobotCommander()
+scene = moveit_commander.PlanningSceneInterface()    
+group = moveit_commander.MoveGroupCommander("arm")
+display_trajectory_publisher = rospy.Publisher('/move_group/display_planned_path', moveit_msgs.msg.DisplayTrajectory, queue_size=1)
+
+"""
+pose_target = geometry_msgs.msg.Pose()
+pose_target.orientation.w = 1.0
+pose_target.position.x = 0.96
+pose_target.position.y = 0
+pose_target.position.z = 1.18
+group.set_pose_target(pose_target)
+"""
+group.set_named_target("home")
+
+plan1 = group.plan()
+
+rospy.sleep(5)
+
+group.go(wait=True)
+
+moveit_commander.roscpp_shutdown()
